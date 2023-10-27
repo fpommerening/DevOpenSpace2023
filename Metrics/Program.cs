345312cb5969e343ac26f127dc35c3a7cf7eccc2
Frank Pommerening <frank@pommerening-online.de>
2023-10-27 13:31:19 +0200

    add samples to metrics playgound

--- Metrics/Program.cs
+++ Metrics/Program.cs
@@ -1,7 +1,22 @@
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using MudBlazor.Services;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenTelemetry().WithMetrics(mb =>
+{
+    //mb.AddRuntimeInstrumentation();
+    mb.AddPrometheusExporter();
+    mb.AddMeter(FP.Monitoring.MetricsPlayground.Pages.Index.MeterName);
+    // mb.AddOtlpExporter(otp =>
+    // {
+    //     otp.Endpoint = new Uri("https://t.container-training.de");
+    // });
+    mb.AddView("playground.histogram", new ExplicitBucketHistogramConfiguration
+    {
+        Boundaries = new double[] { 1, 2, 3, 5 }
+    });
+});
 
 StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
@@ -13,8 +28,8 @@ builder.Services.AddMudServices();
 var app = builder.Build();
 
 app.UseStaticFiles();
-
 app.UseRouting();
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
